71be11b17ad3266e85987be4ee8c35aa13517a42
zxj95121 <986773071@qq.com>
2017-08-22 05:47:29 +0800

    no message

---
+++ app/Http/Controllers/Father/ShareVideoController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Father;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class ShareVideoController extends Controller
+{
+    public function show()
+    {
+    	return view('father.shareVideo');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,7 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::group(['prefix' => 'father','namespace' => 'Father'], function ($router) {
+    $router->get('/shareVideo','ShareVideoController@index');
+});
