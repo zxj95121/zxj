3b5b83304889171a01b14a4cabee6546a4f50cdd
zxj95121 <986773071@qq.com>
2017-09-21 17:44:26 +0800

    nothing

--- public/all/home/js/showCode.js
+++ public/all/home/js/showCode.js
@@ -1,66 +1,15 @@
 $('.contactUl li').each(function(e){
 
-	if (!IsPC()) {
+	$(this).mouseover(function(e){
+		$(this).parents('ul').find('.code').hide();
+		$(this).find('.code').show();
+	});
 
-		$(this)[0].addEventListener('touchstart',touch,false);
+	$(this).mouseout(function(e){
+		$(this).parents('ul').find('.code').hide();
+	});
 
-    	$(this)[0].addEventListener('touchmove',touch,false);
-
-    	$(this)[0].addEventListener('touchend',touch,false);
-		// $(document).on('touchstart', this, function(e){
-		// 	$(this).parents('ul').find('.code').hide();
-		// 	$(this).find('.code').show();
-		// });
-
-		// $(document).on('touchend', this, function(e){
-		// 	$(this).parents('ul').find('.code').hide();
-		// });
-
-	} else {
-
-		$(this).mouseover(function(e){
-			$(this).parents('ul').find('.code').hide();
-			$(this).find('.code').show();
-		});
-
-		$(this).mouseout(function(e){
-			$(this).parents('ul').find('.code').hide();
-		});
-
-	}
-
-	// $(document).click(function(){
-	// 	$('.code').hide();
-	// })
 })
-
-function touch(event) {
-	var event = event || window.event;
-	var target = '#'+event.target.id;
-	switch(event.type){
-
-        case "touchstart":
-
-        	$('.code').hide();
-        	// alert($(target).html());
-        	$(target).find('.code').show();
-        	event.stopPropagation();
-
-        break;
-
-        case "touchend":
-
-            $('.code').hide();
-
-        break;
-
-        case "touchmove":
-
-        	return false;
-
-        break;
-    }
-}
 
 function IsPC() {
     var userAgentInfo = navigator.userAgent;
