24528ffd0b1c5b29c5b2fe25ec147b268349d1e1
zxj95121 <986773071@qq.com>
2017-11-04 21:42:55 +0800

    no message

--- public/minipro/pages/chat/resInvite.js
+++ public/minipro/pages/chat/resInvite.js
@@ -115,7 +115,7 @@ Page({
             url: 'https://api.zhangxianjian.com/pro/chat/isInGroup',
             data: {
                 openid: openid,
-                nickname: nickname,
+                nickName: nickname,
                 gender: gender,
                 avatarUrl: avatarUrl,
                 id: this.data.id,
@@ -173,13 +173,11 @@ Page({
 
     regetInfo: function () {
         var that = this;
-        if (this.data.canIUse) {
-            // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-            // 所以此处加入 callback 以防止这种情况
+        var openid = wx.getStorageSync('openid');
 
-            var openid = wx.getStorageSync('openid');
-
-            app.userInfoReadyCallback = res => {
+        wx.getUserInfo({
+            success: res => {
+                app.globalData.userInfo = res.userInfo
                 this.setData({
                     userInfo: res.userInfo,
                     hasUserInfo: true
@@ -206,11 +204,13 @@ Page({
                     }
                 })
             }
-        }
+        })
+
     },
     //对返回的数据进行处理
     testResult: function (data) {
         console.log(data);
+        var that = this;
         if (data.result == 0) {
             wx.showModal({
               title: '邀请函',
@@ -295,9 +295,10 @@ Page({
               content: data.data+'的微信尚未设置性别',
               success: function(res) {
                 if (res.confirm) {
-                    wx.reLaunch({
+                    /*wx.reLaunch({
                       url: 'main'
-                    })
+                    })*/
+                    that.regetInfo();
                 } else if (res.cancel) {
                     wx.reLaunch({
                       url: 'main'
