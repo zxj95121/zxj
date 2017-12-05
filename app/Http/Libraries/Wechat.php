9a8dd2b14a212bd48efb714d75f9fa64da77af09
zxj95121 <986773071@qq.com>
2017-12-05 21:22:01 +0800

    no message

--- app/Http/Libraries/Wechat.php
+++ app/Http/Libraries/Wechat.php
@@ -68,7 +68,7 @@ class Wechat extends ServiceProvider
     public static function deal_sqlResult($data, $time=false, $desc=false, $column='created_at')
     {
         $result = array();
-        $count = count($result);
+        $count = count($data);
         foreach ($data as $key => $value) {
             if ($desc) {//逆序
                 $key_key = --$count;
