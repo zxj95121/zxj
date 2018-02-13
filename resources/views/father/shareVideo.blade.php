56225ddcf2edc30f1709a12a2e6ff44dd3a200de
zxj95121 <986773071@qq.com>
2018-02-13 12:16:37 +0800

    no message

--- resources/views/father/shareVideo.blade.php
+++ resources/views/father/shareVideo.blade.php
@@ -10,14 +10,14 @@
 		<link href="" rel="stylesheet">
 	<style type="text/css">
 	#modelView{background-color:#DDDDDD;z-index:0;opacity:0.7;height: 100%;width: 100%;position: relative;}
-	.playvideo{padding-top: auto;z-index: 9999;position: relative;top:0;height: 233px;}
+	.playvideo{padding-top: auto;z-index: 9999;position: relative;top:0;height: 232px;}
 	.zy_media{z-index: 999999999}
 	</style>
 <link rel="stylesheet" href="/plugin/mediaPlay/zy.media.min.css">
 	</head>
 	<body>
 	<!-- <body onmousewheel="return false;"> -->
-		<img src="{{$obj['img_url']}}" style="height: 0px;opacity: 0;" alt="视频展示图">
+		<img src="{{$obj['img_url']}}" style="width: 100%;" alt="视频展示图">
 	
 	<div class="playvideo">
 		<div class="zy_media">
@@ -32,7 +32,7 @@
 <script src="/plugin/mediaPlay/zy.media.min.js"></script>
 <script>
 //document.documentElement.style.overflow='hidden';
- document.body.style.overflow='hidden';
+ // document.body.style.overflow='hidden';
     zymedia('video',{autoplay: true});
     var height = document.documentElement.clientHeight;
     var width = document.body.clientWidth;
@@ -41,8 +41,8 @@
     $("#modelView").height(height);
     // var videoheight = $(".zy_media").height()/2;
     // var padding_top = screenheight-videoheight;
-    var padding_top = '100px';
-    $(".playvideo").css({"top":padding_top});
+    // var padding_top = '100px';
+    // $(".playvideo").css({"top":padding_top});
     // $("#modelView").css({"margin-top":-1*(padding_top+$(".zy_media").height())});
 
 </script>
