26e96f4b32c357e6595e8c625c4bde8bd17a2808
wells <xianjian951216@163.com>
2019-07-20 02:38:57 +0800

    修改了hander和诗的meta头部

--- resources/views/poem/poem_single.blade.php
+++ resources/views/poem/poem_single.blade.php
@@ -6,9 +6,7 @@
 	<title>张贤健的诗-{{$poem['title']}}</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=0">
 	<meta name="author" content="张贤健"> 
-	<meta name="keywords" content="张贤健, wells, php, PHP,  张贤健的诗集, {{$poem['title']}}"/>
-	<!-- <meta name="baidu-site-verification" content="lX0mR46Fv3" /> -->
-	<meta name="description" content="张贤健的诗"/>
+	<meta name="keywords" content="张贤健, 张贤健的诗集, {{$poem['title']}}"/>
 	<link rel="shortcut icon" type="image/x-icon" href="/favicon.ico" />
 	
 	<!-- Bootstrap  -->
